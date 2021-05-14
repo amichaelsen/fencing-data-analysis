e4ecfc2380a01a0316a4e26abcf72d86f13114e1
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-14 08:26:25 -0700

    added reloaded WF data and update load script

--- data/load_csv.py
+++ data/load_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 from data.helper.dataframe_columns import FENCERS_RANKINGS_DF_COLS, FENCERS_RANKINGS_MULTI_INDEX
 from data.helper.dataframe_columns import convert_list_to_dataframe_with_multi_index
 
-date = 'May_12_2021'
+date = 'May_13_2021'
 div_name = 'all_womens_foil'
 
 bout_df            = pd.read_csv('data/'+div_name+ '_bout_data_'            +date+'.csv')
@@ -12,4 +12,10 @@ init_rankings_df   = pd.read_csv('data/'+div_name+ '_fencer_rankings_data_' +dat
 
 fencer_rankings_df = convert_list_to_dataframe_with_multi_index(init_rankings_df.values.tolist(), FENCERS_RANKINGS_DF_COLS, FENCERS_RANKINGS_MULTI_INDEX)
 
-# from load_csv import tournament_df, bout_df, fencer_bio_df, fencer_rankings_df
+tournament_df.df_name       = "Tournament Dataframe"
+bout_df.df_name             = "Bouts Dataframe"
+fencer_bio_df.df_name       = "Fencer Bio Dataframe"
+fencer_rankings_df.df_name  = "Fencer Rankings Dataframe"
+
+
+# from data.load_csv import tournament_df, bout_df, fencer_bio_df, fencer_rankings_df
