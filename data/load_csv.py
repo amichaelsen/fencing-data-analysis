b61be6bae8e20e9800cfc8f3312c7141fd4b8959
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-14 14:27:29 -0700

    added data cleanup; add nationality hist to exploration

--- data/load_csv.py
+++ data/load_csv.py
@@ -5,20 +5,21 @@ from data.helper.dataframe_columns import convert_list_to_dataframe_with_multi_i
 date = 'May_13_2021'
 div_name = 'all_womens_foil'
 
-bout_df = pd.read_csv('data/'+div_name + '_bout_data_' + date+'.csv')
-tournament_df = pd.read_csv(
+orig_bout_df = pd.read_csv('data/'+div_name + '_bout_data_' + date+'.csv')
+orig_tournament_df = pd.read_csv(
     'data/'+div_name + '_tournament_data_' + date+'.csv')
-fencer_bio_df = pd.read_csv(
+orig_fencer_bio_df = pd.read_csv(
     'data/'+div_name + '_fencer_bio_data_' + date+'.csv')
 init_rankings_df = pd.read_csv(
     'data/'+div_name + '_fencer_rankings_data_' + date+'.csv')
 
-fencer_rankings_df = convert_list_to_dataframe_with_multi_index(
+orig_fencer_rankings_df = convert_list_to_dataframe_with_multi_index(
     init_rankings_df.values.tolist(), FENCERS_RANKINGS_DF_COLS, FENCERS_RANKINGS_MULTI_INDEX)
 
-tournament_df.df_name = "Tournament Dataframe"
-bout_df.df_name = "Bouts Dataframe"
-fencer_bio_df.df_name = "Fencer Bio Dataframe"
-fencer_rankings_df.df_name = "Fencer Rankings Dataframe"
+orig_tournament_df.df_name = "Tournament Dataframe"
+orig_tournament_df._metadata += ['df_name']
+orig_bout_df.df_name = "Bouts Dataframe"
+orig_fencer_bio_df.df_name = "Fencer Bio Dataframe"
+orig_fencer_rankings_df.df_name = "Fencer Rankings Dataframe"
 
-# from data.load_csv import tournament_df, bout_df, fencer_bio_df, fencer_rankings_df
+# from data.load_csv import orig_tournament_df, orig_bout_df, orig_fencer_bio_df, orig_fencer_rankings_df
