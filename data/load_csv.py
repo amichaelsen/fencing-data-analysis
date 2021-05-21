ee7ac79ba7491542b5438ba2dea3b033c6144baa
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-21 12:01:34 -0700

    added method for loading original data versus cleaned data

--- data/load_csv.py
+++ data/load_csv.py
@@ -23,3 +23,24 @@ orig_fencer_bio_df.df_name = "Fencer Bio Dataframe"
 orig_fencer_rankings_df.df_name = "Fencer Rankings Dataframe"
 
 # from data.load_csv import orig_tournament_df, orig_bout_df, orig_fencer_bio_df, orig_fencer_rankings_df
+
+
+
+cleaned_bout_df = pd.read_csv('data/cleaned_data/'+div_name + '_bout_data_' + date+'_cleaned.csv')
+cleaned_tournament_df = pd.read_csv(
+    'data/cleaned_data/'+div_name + '_tournament_data_' + date+'_cleaned.csv')
+cleaned_fencer_bio_df = pd.read_csv(
+    'data/cleaned_data/'+div_name + '_fencer_bio_data_' + date+'_cleaned.csv')
+init_cleaned_rankings_df = pd.read_csv(
+    'data/cleaned_data/'+div_name + '_fencer_rankings_data_' + date+'_cleaned.csv')
+
+cleaned_fencer_rankings_df = convert_list_to_dataframe_with_multi_index(
+    init_cleaned_rankings_df.values.tolist(), FENCERS_RANKINGS_DF_COLS, FENCERS_RANKINGS_MULTI_INDEX)
+
+cleaned_tournament_df.df_name = "Tournament Dataframe"
+cleaned_tournament_df._metadata += ['df_name']
+cleaned_bout_df.df_name = "Bouts Dataframe"
+cleaned_fencer_bio_df.df_name = "Fencer Bio Dataframe"
+cleaned_fencer_rankings_df.df_name = "Fencer Rankings Dataframe"
+
+# from data.load_csv import cleaned_tournament_df, cleaned_bout_df, cleaned_fencer_bio_df, cleaned_fencer_rankings_df
