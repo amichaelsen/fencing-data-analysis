878a032d53467f2eb62c33d76bd9417d9974f6a9
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-21 10:09:55 -0700

    relocated input data to an initial data subdirectory

--- data/load_csv.py
+++ data/load_csv.py
@@ -5,13 +5,13 @@ from data.helper.dataframe_columns import convert_list_to_dataframe_with_multi_i
 date = 'May_13_2021'
 div_name = 'all_womens_foil'
 
-orig_bout_df = pd.read_csv('data/'+div_name + '_bout_data_' + date+'.csv')
+orig_bout_df = pd.read_csv('data/initial_data/'+div_name + '_bout_data_' + date+'.csv')
 orig_tournament_df = pd.read_csv(
-    'data/'+div_name + '_tournament_data_' + date+'.csv')
+    'data/initial_data/'+div_name + '_tournament_data_' + date+'.csv')
 orig_fencer_bio_df = pd.read_csv(
-    'data/'+div_name + '_fencer_bio_data_' + date+'.csv')
+    'data/initial_data/'+div_name + '_fencer_bio_data_' + date+'.csv')
 init_rankings_df = pd.read_csv(
-    'data/'+div_name + '_fencer_rankings_data_' + date+'.csv')
+    'data/initial_data/'+div_name + '_fencer_rankings_data_' + date+'.csv')
 
 orig_fencer_rankings_df = convert_list_to_dataframe_with_multi_index(
     init_rankings_df.values.tolist(), FENCERS_RANKINGS_DF_COLS, FENCERS_RANKINGS_MULTI_INDEX)
